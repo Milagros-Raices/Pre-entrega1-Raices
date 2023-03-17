80975be390cfaf5f41d4cc551b66754c4eb7245c
Milagros Raices <milagrosraices2000@gmail.com>
2023-03-17 20:13:41 -0300

    Simulacro interactivo-arreglos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 function simulacro() {
+//Algoritmo con ciclos
+let agregarAlCarrito = true; 
+let precioFinal = 0;
+
+while(agregarAlCarrito) {    
+    let precio = parseInt(prompt("Agregar precio al carrito, si no queres agregar mas escribir 0"));
+    if (precio == 0) {
+        break;
+    }else {
+        precioFinal = precioFinal + precio;
+    }
+}
+
+alert("El precio final es: $" + precioFinal);
+
+//Algoritmo condicional 
     let cantidadDeProductos = Math.random () > 0.5;
     
-    if (cantidadDeProductos) {       //Algoritmo condicional 
+    if (cantidadDeProductos) {      
         let medioDePago = prompt("Desea pagar con: Mercado Pago, Tarjeta Debito, Tarjeta Credito").toLocaleLowerCase(); 
         if (medioDePago == "mercado pago") {
             alert("Usted realizo el pago con Mercado Pago");
@@ -16,20 +32,6 @@ function simulacro() {
         alert("Ups! No se ha podido realizar la compra ya que no hay ningun producto en el carrito");
     }
     
-    
-    let agregarAlCarrito = true; 
-    let precioFinal = 0;
-    
-    while(agregarAlCarrito) {      //Algoritmo con ciclos
-        let precio = parseInt(prompt("Agregar precio al carrito, si no queres agregar mas escribir 0"));
-        if (precio == 0) {
-            break;
-        }else {
-            precioFinal = precioFinal + precio;
-        }
-    }
-    
-    alert("El precio final es: $" + precioFinal);
 }
 
 simulacro();
